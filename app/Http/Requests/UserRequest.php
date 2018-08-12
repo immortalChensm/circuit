344571907bcffcba7541df2d9afe168685c58ab8
oldshiji <1655664358@qq.com>
2018-08-12 16:12:20 +0800

    user can upload avatar

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,7 +28,8 @@ class UserRequest extends FormRequest
             //
             'name'=>'required|between:3,25|regex:/^[\w+]+$/|unique:users,name,'.Auth::id(),
             'email'=>'required|email',
-            'introduction'=>'required|max:80'
+            'introduction'=>'required|max:80',
+            'avatar'=>'mimes:jpg,jpeg,gif,png|dimensions:min_width=200,min_height=200',
         ];
     }
 
@@ -38,7 +39,9 @@ class UserRequest extends FormRequest
            "name.required"=>"用户名要填写",
            "name.between"=>"用户名3到25个字",
            "name.unique"=>"用户名已经存在",
-           "name.regex"=>"用户名只支持英文"
+           "name.regex"=>"用户名只支持英文",
+           "avatar.mimes"=>"图片只有是jpg,jpeg,gif,png才可以",
+           "avatar.dimensions"=>"只能上传200*200的图片"
        ];
     }
 }
