f9f619ac201e43b694c8535687be0eb399b4a73f
oldshiji <1655664358@qq.com>
2018-08-12 15:23:21 +0800

    basic user profile editing

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+use Auth;
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            'name'=>'required|between:3,25|regex:/^[\w+]+$/|unique:users,name,'.Auth::id(),
+            'email'=>'required|email',
+            'introduction'=>'required|max:80'
+        ];
+    }
+
+    public function messages()
+    {
+       return [
+           "name.required"=>"用户名要填写",
+           "name.between"=>"用户名3到25个字",
+           "name.unique"=>"用户名已经存在",
+           "name.regex"=>"用户名只支持英文"
+       ];
+    }
+}
