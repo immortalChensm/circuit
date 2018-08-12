ced037bcb56969ff897bc5cec3c07361a530c720
oldshiji <1655664358@qq.com>
2018-08-12 12:47:16 +0800

    finish user auth

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PagesController extends Controller
+{
+    //
+
+    public function root()
+    {
+        
+        return view("pages.root");
+    }
+}
