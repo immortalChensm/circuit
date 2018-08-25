3a13891c1f713cc5cc189d5b78e336f9a9cc99ed
oldshiji <1655664358@qq.com>
2018-08-25 17:57:24 +0800

    bbs

--- app/Http/Controllers/JackController.php
+++ app/Http/Controllers/JackController.php
@@ -6,11 +6,13 @@ use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use Oldshiji\JackTest\Test;
 
 class JackController extends Controller
 {
     public function index()
     {
-        echo 'jack';
+        $test = new Test();
+        echo $test->get();
     }
 }
--- config/services.php
+++ config/services.php
@@ -37,6 +37,6 @@ return [
     'baidu_translate' => [
         'appid' => env('BAIDU_TRANSLATE_APPID'),
         'key'   => env('BAIDU_TRANSLATE_KEY'),
-    ],
+    ]
 
 ];
